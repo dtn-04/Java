040246074924e537f0119d2977fdfb0087e7856a
Đỗ Thị Ngân <nganthido483@gmail.com>
2025-07-19 20:33:09 +0700

    variables

---
+++ Day1/variables.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class variables {
+    public static void main(String[] args){
+
+        String name = "Đỗ Thị Ngân";
+        int gta = 21;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        System.out.println("My name is: " + name);
+        System.out.println("Gta: " + gta);
+        System.out.println("Pi: " + pi);
+        System.out.println("Gender: " + gender);
+        System.out.println("Is admin: " + isAdmin);
+
+    }
+}
