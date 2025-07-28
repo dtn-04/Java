d5c9a1d8c3072702b196e2d2a85bd00a55e499ee
Đỗ Thị Ngân <nganthido483@gmail.com>
2025-07-28 16:15:56 +0700

    Slots machine

--- Day8/Emoji.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
-public class Emoji {
-    public static void main(String[] args) throws Exception {
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
-        System.out.println("🍒 🍋 🍉");
-    }
-}
