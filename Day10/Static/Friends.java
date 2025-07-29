a629fd7914b6d4eb06142c8dfcb3443196e89a1b
Đỗ Thị Ngân <nganthido483@gmail.com>
2025-07-29 23:55:16 +0700

    Static

---
+++ Day10/Static/Friends.java
@@ -0,0 +1,13 @@
+package Day10.Static;
+
+public class Friends {
+    static int numOfFriends;
+    String name;
+
+    public Friends(String name) {
+        this.name = name;
+        numOfFriends++;
+    }
+
+    
+}
---
+++ Day10/Static/Main.java
@@ -0,0 +1,11 @@
+package Day10.Static;
+
+public class Main {
+    public static void main(String[] args) {
+        Friends friends1 = new Friends("nganz");
+        Friends friends2 = new Friends("a");
+        Friends friends3 = new Friends("b");
+
+        System.out.println(Friends.numOfFriends);
+    }
+}
